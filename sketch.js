44d2a96a560d8ef9528675736d5f1b88b0a06edb
Isa <70447522+Cattato999@users.noreply.github.com>
2020-10-05 17:22:55 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -50,7 +50,6 @@ function draw() {
   background(0);
   packageSprite.x= packageBody.position.x 
   packageSprite.y= packageBody.position.y 
-  packageSprite.display();
   drawSprites();
  
 }
@@ -58,8 +57,7 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
-		packageSprite.x=packageBody.position.x
-		packageSprite.y=packageBody.position.y
+		Matter.Body.setStatic(packageBody,false);
 	}
   }
 
